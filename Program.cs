b5bc7ba3edb15c3e2e5ba93bac2470ff4fd1df2f
user <user@LAB>
2024-05-03 11:25:10 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace imaginebeingsoassatthis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 345;
+            int b = 1143;
+            Console.WriteLine(a + b);
+        }
+    }
+}
